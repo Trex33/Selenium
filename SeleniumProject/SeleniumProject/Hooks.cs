f5dbc3a219313ef81986c179e3079fc1879df67e
Károly Horváth <hkaroly33@gmail.com>
2018-07-14 20:06:29 +0200

    First

---
+++ SeleniumProject/SeleniumProject/Hooks.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumProject
+{
+    public class Hooks : Base
+    {
+        public Hooks()
+        {
+            Driver = new FirefoxDriver();
+        }
+    }
+}
---
+++ SeleniumProject/SeleniumProject/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SeleniumProject
+{
+    [TestFixture]
+    public class FirefoxTesting : Hooks
+    {
+        [Test]
+        public void FirefoxWikiTest()
+        {
+            Driver.Navigate().GoToUrl("https://www.wikipedia.org");
+            Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
+                                                                                "asd");
+        }
+    }
+
+    [TestFixture]
+    public class ChromeTesting : Hooks
+    {
+        [Test]
+        public void ChromeWikiTest()
+        {
+            Driver.Navigate().GoToUrl("https://www.wikipedia.org");
+            Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
+                                                                                "asd");
+        }
+    }
+}
